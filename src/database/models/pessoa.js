5b230e31bf0e05caf5313d25c4f76d91a80b083b
Andre Michels de Oliveira <michelsandre@gmail.com>
2024-01-02 15:57:30 -0300

    add validacoes

--- src/database/models/pessoa.js
+++ src/database/models/pessoa.js
@@ -1,4 +1,6 @@
 "use strict";
+const isCpfValido = require("../../utils/validaCpfHelper.js");
+
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Pessoa extends Model {
@@ -21,9 +23,32 @@ module.exports = (sequelize, DataTypes) => {
   }
   Pessoa.init(
     {
-      nome: DataTypes.STRING,
-      email: DataTypes.STRING,
-      cpf: DataTypes.STRING,
+      nome: {
+        type: DataTypes.STRING,
+        validate: {
+          len: {
+            args: [3, 50],
+            msg: "O campo nome dever ter no mínimo 3 caracteres",
+          },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        validate: {
+          isEmail: {
+            args: true,
+            msg: "Formato do email inválido",
+          },
+        },
+      },
+      cpf: {
+        type: DataTypes.STRING,
+        validate: {
+          cpfEhValido: (cpf) => {
+            if (!isCpfValido(cpf)) throw new Error("numero de CPF invalido");
+          },
+        },
+      },
       ativo: DataTypes.BOOLEAN,
       role: DataTypes.STRING,
     },
